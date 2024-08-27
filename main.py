6c196b9a66fbf4b8812d1c2a268913faeb23714d
stick <stick@ptr-4xd7xrd0uiehp7jyvxw.18120a2.ip6.access.telenet.be>
2024-08-27 18:06:11 +0200

    player done

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
 
     while True:
@@ -16,8 +21,10 @@ def main():
                 return
 
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        for item in updatable:
+            item.update(dt)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
