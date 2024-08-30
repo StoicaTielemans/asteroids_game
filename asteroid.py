b805350daf8f2e998c5f9bc1b9d2568e6a15277e
StoicaTielemans <r0983518@student.thomasmore.be>
2024-08-30 03:11:33 +0200

    bullets now split big asteroid into smaller ones

--- asteroid.py
+++ asteroid.py
@@ -14,3 +14,17 @@ class Asteroid(CircleShape):
     def update(self, dt):
         self.position += self.velocity * dt
 
+    def split(self):
+        self.kill()
+        if self.radius < ASTEROID_MIN_RADIUS:
+            return
+        else:
+            random_angle = random.uniform(20, 50)
+            new_velocity1 = self.velocity.rotate(random_angle)
+            new_velocity2 = self.velocity.rotate(-random_angle)
+            asteroid1 = Asteroid(
+                self.position.x, self.position.y, self.radius - ASTEROID_MIN_RADIUS)
+            asteroid1.velocity = new_velocity1 * 1.2
+            asteroid2 = Asteroid(
+                self.position.x, self.position.y, self.radius - ASTEROID_MIN_RADIUS)
+            asteroid2.velocity = new_velocity2 * 1.2
